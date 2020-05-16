c20e678001578f52e228d1fe748a0f82900d0293
Edgar Munro <github-code@elmunro.me>
2020-05-16 17:10:22 +0200

    Finished

--- browser.py
+++ browser.py
@@ -1,44 +1,58 @@
 from collections import deque
 from pathlib import Path
 import requests
+from bs4 import BeautifulSoup
 
 import os
 import sys
+from colorama import init, Fore, Style, Back
 
 
-def fetch_local_store(site_name: str):
-    if site_name in sites.keys():
-        with open(storage + f"/{site_name}", 'r') as f:
-            print(f.read())
-        history.append(site_name)
-    else:
-        print("Error: Not Found in local database...")
+def print_text_from_html(html):
+    init(autoreset=True)
+    soup = BeautifulSoup(html, 'html.parser')
+    text_tags = soup.find_all(['a', 'p', 'ul', 'li', 'ol'])
+    for t in text_tags:
+        if t.name == 'a':
+            text = t.get_text()
+            print(Fore.BLUE + text)
+        else:
+            print(t.get_text())
 
 
 history = deque()
-
 storage = sys.argv[1]
 Path(storage).mkdir(exist_ok=True)
 local_sites = {}
+
+for file in os.listdir(storage):
+    if os.path.isfile(os.path.join(storage, file)):
+        name = '.'.join(file.split('.')[:-1])
+        print("Found local: " + name)
+        local_sites[name] = file
+
+
 while True:
     input_str = input()
     if input_str == 'exit':
         exit(0)
     elif input_str == 'back':
-        with open(storage + f"/{history.pop()}", 'r') as f:
-            print(f.read())
+        with open(storage + f"/{history.pop()}", 'rb') as f:
+            print_text_from_html(f.read())
     elif input_str in local_sites.keys():
-        with open(storage+f"/{local_sites[input_str]}", 'r') as f :
-            print(f.read())
+        with open(storage + f"/{local_sites[input_str]}", 'rb') as f:
+            print_text_from_html(f.read())
     else:
         site_name = '.'.join(input_str.split('://')[-1].split('.')[:-1])
         clean_url = input_str.split('://')[-1]
         try:
-            text = requests.get('https://' + clean_url).text
+            page = requests.get('https://' + clean_url)
             history.append(site_name)
             local_sites[site_name] = clean_url
-            print(text)
-            with open(storage + f"/{clean_url}", 'w') as f:
-                f.write(text)
+
+            print_text_from_html(page.content)
+            with open(storage + f"/{clean_url}", 'wb') as f:
+                f.write(page.content)
+
         except requests.RequestException:
             print('Error, not found')
